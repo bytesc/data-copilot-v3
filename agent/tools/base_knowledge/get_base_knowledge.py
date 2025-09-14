54d8ed4feab486469001b87d0787af52d6dc14f2
rcy <21010078@mail.ecust.edu.cn>
2025-09-14 20:00:10 +0800

    table selection

--- agent/tools/base_knowledge/get_base_knowledge.py
+++ agent/tools/base_knowledge/get_base_knowledge.py
@@ -1,14 +1,3 @@
 def get_base_knowledge(key=""):
-    knowledge = """
-We have two different Dataset:    
-    
-# BRSET Dataset Overview
-## Introduction
-The BRSET (Brazilian Multilabel Ophthalmological Dataset) was developed by researchers from São Paulo Federal University and Massachusetts Institute of Technology to advance machine learning research in ophthalmology. BRSET represents the first publicly available Brazilian multilabel ophthalmological dataset, offering a comprehensive resource for developing AI models to predict demographic characteristics and multi-label disease classification using retinal fundus photos. The dataset addresses the critical need for representative data from low and middle-income countries to reduce bias in ophthalmological AI systems.
-
-# mBRSET Dataset Overview
-## Introduction
-The mBRSET (Mobile Brazilian Retinal Dataset) was developed by researchers from Sao Paulo Federal University and Massachusetts Institute of Technology, to support research in portable diabetic retinopathy (DR) screening. mBRSET dataset represents the first publicly available collection of retinal images captured via handheld retinal cameras in real-life, high-burden scenarios, offering a comprehensive resource for addressing the challenges of diabetic retinopathy screening and management in underserved populations.
-    """
     knowledge = ""
     return knowledge
